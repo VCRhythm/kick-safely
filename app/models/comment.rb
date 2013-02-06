e5ee41f6630dc2a1b41f047b8c56c680161ad45e
Tristan Hardy <tristan@tristanhardy.com>
2013-02-06 13:01:55 -0500

    Structure in place

---
+++ app/models/comment.rb
@@ -0,0 +1,17 @@
+# == Schema Information
+#
+# T:browse confirm e
+# able name: comments
+#
+#  id         :integer          not null, primary key
+#  content    :string(255)
+#  user_id    :integer
+#  project_id :integer
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
+class Comment < ActiveRecord::Base
+  belongs_to :user
+  validates :content, :length=>{:maximum=>140}
+end
